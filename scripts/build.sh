51e223a5dbfc4f314f945fb31ba10cbec6472cf6
yboyer <bhyw180@gmail.com>
2016-10-01 15:36:20 +0200

    Fix builder and deployer

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd "$(dirname "$0")" #CWD
 
+# Build the client
+(cd ../client && npm run build)
+
+echo
+echo \# Build image...
 # Build the Docker image
 docker build -t yboyer/winn-project ..
+
+echo
+# Display the yboyer/winn-project image
+echo \# Image builded
+docker images yboyer/winn-project
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,19 @@
 cd "$(dirname "$0")" #CWD
 
 # Retreive image from the Docker HUB
+echo \# Pulling...
 docker pull yboyer/winn-project
+
+echo
 # Start or restart the server
-docker run --restart=always -d yboyer/winn-project
-# or to exposed port to the host interface, run:
-# docker run --restart=always -p 3000:3000 -d yboyer/winn-project
+echo \# Running...
+docker stop yboyerwt
+docker rm yboyerwt
+docker run --name=yboyerwt --restart=always -d yboyer/winn-project
+# or to expose port to the host interface, run:
+# docker run --name=yboyerwt --restart=always -p 3000:3000 -d yboyer/winn-project
+
+echo
+# Display the yboyer/winn-project containers
+echo \# Container started
+docker ps -f ancestor=yboyer/winn-project
